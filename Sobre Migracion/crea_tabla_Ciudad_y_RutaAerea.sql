f8b448a5bd6a5213897f992bf1740a47b270927f
JuanchiRios <juanchirios04@hotmail.com>
2015-10-19 20:42:31 -0300

    mis 8 tablas vacias comen trigo

--- Sobre Migracion/crea_tabla_Ciudad_y_RutaAerea.sql
+++ Sobre Migracion/crea_tabla_Ciudad_y_RutaAerea.sql
@@ -24,3 +24,81 @@ INSERT INTO gd_ñuflo.Ciudad (nombre)
 	UNION
 	SELECT Ruta_Ciudad_Destino FROM GD2C2015.gd_esquema.Maestra
 	GROUP BY Ruta_Ciudad_Destino
+
+
+CREATE TABLE gd_ñuflo.Producto (
+	id_producto INT PRIMARY KEY,
+	millas_necesarias INT,
+	stock INT,
+	descripcion NVARCHAR(510),
+	)
+
+CREATE TABLE gd_ñuflo.Canje (
+	id_canje INT PRIMARY KEY,
+	id_cliente INT REFERENCES gd_ñuflo.Cliente,
+	id_Producto INT REFERENCES gd_ñuflo.Producto,
+	cantidad INT,
+	fecha_de_canje DATETIME,
+	)
+
+
+CREATE TABLE gd_ñuflo.Milla (
+	id_milla INT PRIMARY KEY,
+	id_cliente INT REFERENCES gd_ñuflo.Cliente,
+	fecha_de_obtencion DATETIME,
+	cantidad INT,
+	)
+
+
+CREATE TABLE gd_ñuflo.Cliente (
+	id_cliente INT PRIMARY KEY,
+	dni numeric(18, 0),
+	nombre nvarchar(255),
+	apellido nvarchar(255),
+	direccion nvarchar(255),
+	telefono numeric(18, 0),
+	mail nvarchar(255),
+	fecha_de_nacimiento DATETIME,
+	)
+
+
+
+CREATE TABLE gd_ñuflo.PasajeEncomienda (
+	id_pasaje_encomienda INT PRIMARY KEY,
+	codigo_de_compra  INT REFERENCES gd_ñuflo.Compra,
+	id_cliente  INT REFERENCES gd_ñuflo.Cliente,
+	peso_encomienda numeric(18, 0),
+	numero_de_butaca numeric(18, 0),
+	cancelado BIT,
+	)
+
+CREATE TABLE gd_ñuflo.PasajeEncomienda (
+	id_pasaje_encomienda INT PRIMARY KEY,
+	codigo_de_compra  INT REFERENCES gd_ñuflo.Compra,
+	id_cliente  INT REFERENCES gd_ñuflo.Cliente,
+	peso_encomienda numeric(18, 0),
+	numero_de_butaca numeric(18, 0),
+	cancelado BIT,
+	)
+
+
+CREATE TABLE gd_ñuflo.PasajeEncomiendaCancelacion (
+	id_pasaje_encomienda INT REFERENCES gd_ñuflo.PasajeEncomienda,
+	id_cancelacion INT REFERENCES gd_ñuflo.Cancelacion,
+	motivo_cancelacion  nvarchar(255),
+	PRIMARY KEY (id_cancelacion, id_pasaje_encomienda)
+	)
+
+
+CREATE TABLE gd_ñuflo.PasajeEncomiendaCancelacion (
+	id_cancelacion INT PRIMARY KEY,
+	codigo_de_compra INT REFERENCES gd_ñuflo.Compra,
+	)
+
+
+CREATE TABLE gd_ñuflo.Compra (
+	codigo_de_compra INT PRIMARY KEY,
+	id_viaje INT /*REFERENCES gd_ñuflo.Viaje*/,
+	id_cliente INT REFERENCES gd_ñuflo.Cliente,
+	fecha_de_compra DATETIME,
+	)
