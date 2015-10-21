3c50a32168f78a408b17b5aa5c2cdf48ce250e83
Ramiro Vazquez <ramirov93@gmail.com>
2015-10-21 16:33:25 -0300

    Ultima prueba de mi parte en cuanto a lo de la Ñ. Además agregué una coma que le faltaba a una create table y le saque una coma que sobraba en otro.

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -1,4 +1,4 @@
-USE GD2C2015
+﻿USE [GD2C2015]
 GO
 
 SET ANSI_NULLS ON
@@ -6,28 +6,28 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-/****CREACION DE SCHEMA****/
-CREATE SCHEMA NIUFLO AUTHORIZATION gd
+CREATE SCHEMA [ÑUFLO] AUTHORIZATION gd
 GO
 
+
 /****CREACION DE TABLAS****/
-CREATE TABLE NIUFLO.Ciudad (
+CREATE TABLE ÑUFLO.Ciudad (
 	id_ciudad int IDENTITY(1,1) PRIMARY KEY,
 	nombre nvarchar(255)
 	)
 GO
 
-CREATE TABLE NIUFLO.Ruta_Aerea (
+CREATE TABLE ÑUFLO.Ruta_Aerea (
 	id_ruta int IDENTITY(1,1) PRIMARY KEY,
-	id_ciudad_origen int REFERENCES NIUFLO.Ciudad,
-	id_ciudad_destino int REFERENCES NIUFLO.Ciudad,
+	id_ciudad_origen int REFERENCES ÑUFLO.Ciudad,
+	id_ciudad_destino int REFERENCES ÑUFLO.Ciudad,
 	precio_base_por_peso numeric(18,2),
-	precio_base_por_pasaje numeric(18,2)
+	precio_base_por_pasaje numeric(18,2),
 	tipo_servicio nvarchar(255)
 	)
 GO
 	
-CREATE TABLE NIUFLO.Aeronave (
+CREATE TABLE ÑUFLO.Aeronave (
 	id_aeronave int IDENTITY(1,1) PRIMARY KEY,
 	modelo nvarchar(255),
 	matricula nvarchar(255) UNIQUE,
@@ -35,41 +35,41 @@ CREATE TABLE NIUFLO.Aeronave (
 	tipo_de_servicio nvarchar(255),
 	fecha_de_alta datetime,
 	capacidad_peso_encomiendas numeric(18,0),
-	baja_vida_utill datetime,
+	baja_vida_utill datetime
 	)
 GO
 	
-CREATE TABLE NIUFLO.ButacaPorAvion (
-	id_aeronave int REFERENCES NIUFLO.Aeronave,
-	id_butaca int REFERENCES NIUFLO.Ruta_Aerea,
+CREATE TABLE ÑUFLO.ButacaPorAvion (
+	id_aeronave int REFERENCES ÑUFLO.Aeronave,
+	id_butaca int REFERENCES ÑUFLO.Ruta_Aerea,
 	ocupada bit,
 	PRIMARY KEY(id_aeronave, id_butaca)
 	)
 GO
 
-CREATE TABLE NIUFLO.Butaca (
+CREATE TABLE ÑUFLO.Butaca (
 	id_butaca int PRIMARY KEY,
 	numero_de_butaca numeric(18,0),
 	tipo_butaca nvarchar(255)
 	)
 GO
 	
-CREATE TABLE NIUFLO.ServicioTecnico (
+CREATE TABLE ÑUFLO.ServicioTecnico (
 	id_servicio int PRIMARY KEY,
-	id_aeronave int REFERENCES NIUFLO.Aeronave,
+	id_aeronave int REFERENCES ÑUFLO.Aeronave,
 	fecha_fuera_de_servicio datetime,
 	fecha_reinicio_de_servicio datetime
 	)
 GO
 
-ALTER TABLE NIUFLO.Aeronave
-	ADD baja_por_fuera_de_servicio int REFERENCES NIUFLO.ServicioTecnico
+ALTER TABLE ÑUFLO.Aeronave
+	ADD baja_por_fuera_de_servicio int REFERENCES ÑUFLO.ServicioTecnico
 GO
 	
-CREATE TABLE NIUFLO.Viaje (
+CREATE TABLE ÑUFLO.Viaje (
 	id_viaje int IDENTITY(1,1) PRIMARY KEY,
-	id_aeronave int REFERENCES NIUFLO.Aeronave,
-	id_ruta int REFERENCES NIUFLO.Ruta_Aerea,
+	id_aeronave int REFERENCES ÑUFLO.Aeronave,
+	id_ruta int REFERENCES ÑUFLO.Ruta_Aerea,
 	peso_ocupado numeric(18,0),
 	fecha_salida datetime,
 	fecha_llegada datetime,
@@ -77,7 +77,7 @@ CREATE TABLE NIUFLO.Viaje (
 	)
 GO
 
-CREATE TABLE NIUFLO.Cliente (
+CREATE TABLE ÑUFLO.Cliente (
 	id_cliente int PRIMARY KEY,
 	dni numeric(18, 0),
 	nombre nvarchar(255),
@@ -89,15 +89,15 @@ CREATE TABLE NIUFLO.Cliente (
 	)
 GO
 
-CREATE TABLE NIUFLO.Milla (
+CREATE TABLE ÑUFLO.Milla (
 	id_milla int PRIMARY KEY,
-	id_cliente int REFERENCES NIUFLO.Cliente,
+	id_cliente int REFERENCES ÑUFLO.Cliente,
 	fecha_de_obtencion datetime,
 	cantidad int
 	)
 GO
 	
-CREATE TABLE NIUFLO.Producto (
+CREATE TABLE ÑUFLO.Producto (
 	id_producto int PRIMARY KEY,
 	millas_necesarias int,
 	stock int,
@@ -105,42 +105,42 @@ CREATE TABLE NIUFLO.Producto (
 	)
 GO
 
-CREATE TABLE NIUFLO.Canje (
+CREATE TABLE ÑUFLO.Canje (
 	id_canje int PRIMARY KEY,
-	id_cliente int REFERENCES NIUFLO.Cliente,
-	id_Producto int REFERENCES NIUFLO.Producto,
+	id_cliente int REFERENCES ÑUFLO.Cliente,
+	id_Producto int REFERENCES ÑUFLO.Producto,
 	cantidad int,
 	fecha_de_canje datetime
 	)
 GO
 
-CREATE TABLE NIUFLO.Compra (
+CREATE TABLE ÑUFLO.Compra (
 	codigo_de_compra int PRIMARY KEY,
-	id_viaje int REFERENCES NIUFLO.Viaje,
-	id_cliente int REFERENCES NIUFLO.Cliente,
+	id_viaje int REFERENCES ÑUFLO.Viaje,
+	id_cliente int REFERENCES ÑUFLO.Cliente,
 	fecha_de_compra datetime
 	)
 GO
 	
-CREATE TABLE NIUFLO.PasajeEncomienda (
+CREATE TABLE ÑUFLO.PasajeEncomienda (
 	id_pasaje_encomienda numeric(18,0) PRIMARY KEY,
-	codigo_de_compra  int REFERENCES NIUFLO.Compra,
-	id_cliente  int REFERENCES NIUFLO.Cliente,
+	codigo_de_compra  int REFERENCES ÑUFLO.Compra,
+	id_cliente  int REFERENCES ÑUFLO.Cliente,
 	peso_encomienda numeric(18, 0),
 	numero_de_butaca numeric(18, 0),
 	cancelado bit
 	)
 GO
 
-CREATE TABLE NIUFLO.Cancelacion (
+CREATE TABLE ÑUFLO.Cancelacion (
 	id_cancelacion int PRIMARY KEY,
-	codigo_de_compra int REFERENCES NIUFLO.Compra
+	codigo_de_compra int REFERENCES ÑUFLO.Compra
 	)
 GO
 
-CREATE TABLE NIUFLO.PasajeEncomiendaPorCancelacion (
-	id_pasaje_encomienda numeric(18,0) REFERENCES NIUFLO.PasajeEncomienda,
-	id_cancelacion int REFERENCES NIUFLO.Cancelacion,
+CREATE TABLE ÑUFLO.PasajeEncomiendaPorCancelacion (
+	id_pasaje_encomienda numeric(18,0) REFERENCES ÑUFLO.PasajeEncomienda,
+	id_cancelacion int REFERENCES ÑUFLO.Cancelacion,
 	motivo_cancelacion  nvarchar(255),
 	PRIMARY KEY (id_cancelacion, id_pasaje_encomienda)
 	)
@@ -148,7 +148,7 @@ GO
 
 /**** MIGRACION *****/
 
-INSERT INTO NIUFLO.Ciudad (nombre) 
+INSERT INTO ÑUFLO.Ciudad (nombre) 
 	SELECT (Ruta_Ciudad_Origen) FROM gd_esquema.Maestra
 	GROUP BY Ruta_Ciudad_Origen
 	UNION
