c6b3fc549764c0e17ebe02aee3fc2fe44c83a87b
Ramiro Vazquez <ramirov93@gmail.com>
2015-10-21 20:00:09 -0300

    Formularios de ABM Rol incluyendo los formularios de selección para modificación, modificación y baja. Además de incluir los componentes que creo se van a necesitar, implementé en un ejemplo de prueba el funcionamiento del dataGridView agregándole el botón Seleccionar a los datos traídos de la base de datos. Cuando se preciona éste, redirige al formulario modificación y le pasa los valores del registro (rol) que se desea modificar, para que el usuario cambie los que quiera y de Guardar.

---
+++ src/AerolineaFrba/Abm Rol/FormSeleccionRol.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm_Rol
+{
+    public partial class FormSeleccionRol : Form
+    {
+        public FormSeleccionRol()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            textBoxNombre.Clear();
+            comboBoxFuncionalidad.ResetText();
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView.ColumnCount - 1) //la última columna sería la de "Seleccionar"
+            {
+                List<String> registroSeleccionado = new List<string>();
+
+                for (int i = 0; i < dataGridView.ColumnCount; i++)
+                {
+                    registroSeleccionado.Add(dataGridView.Rows[e.RowIndex].Cells[i].FormattedValue.ToString());
+                }
+
+                FormModificacionRol fmr = new FormModificacionRol(registroSeleccionado);
+                fmr.Show();
+            }
+        }
+
+        private void FormSeleccionRol_Load(object sender, EventArgs e)
+        {
+            //Datos de prueba, ya que no tengo la BD todavía. Quiero ver si puedo
+            //pasar estos datos al formulario Modificacion, como tendría que hacer la aplicación
+            //con los datos de la tabla verdadera de roles
+            dataGridView.Rows.Add(1);
+            dataGridView.Rows[0].Cells[0].Value = "Nombre de Rol";
+            dataGridView.Rows[0].Cells[1].Value = "Una Funcionalidad de Rol";
+            dataGridView.Rows[1].Cells[0].Value = "Otro nombre de Rol";
+            dataGridView.Rows[1].Cells[1].Value = "Una Funcionalidad de Otro Rol";
+        }
+    }
+}
--- src/AerolineaFrba/Form1.Designer.cs
+++ src/AerolineaFrba/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnAltaRuta = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAltaRol
             // 
-            this.btnAltaRol.Location = new System.Drawing.Point(26, 111);
+            this.btnAltaRol.Location = new System.Drawing.Point(26, 105);
             this.btnAltaRol.Name = "btnAltaRol";
             this.btnAltaRol.Size = new System.Drawing.Size(92, 23);
             this.btnAltaRol.TabIndex = 0;
@@ -72,11 +74,33 @@
             this.btnAltaRuta.UseVisualStyleBackColor = true;
             this.btnAltaRuta.Click += new System.EventHandler(this.btnAltaRuta_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ir modificar a rol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Ir a Baja Rol";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 328);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAltaRuta);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -94,6 +118,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnAltaRuta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
