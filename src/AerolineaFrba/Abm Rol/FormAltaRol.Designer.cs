1ab87b0455c506de7add31454b89947f0e122a9e
j9794 <juanmanuel9794@hotmail.com>
2015-10-22 18:15:23 -0300

    Arreglando bugs de ramiro.

--- src/AerolineaFrba/Abm Rol/FormAltaRol.Designer.cs
+++ src/AerolineaFrba/Abm Rol/FormAltaRol.Designer.cs
@@ -118,7 +118,7 @@
             this.AcceptButton = this.btnGuardar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
             this.ClientSize = new System.Drawing.Size(445, 317);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.btnLimpiar);
