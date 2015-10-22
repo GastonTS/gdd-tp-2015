b7c6a6b18b3957690b00dff27000493651c76daf
Ramiro Vazquez <ramirov93@gmail.com>
2015-10-22 17:39:12 -0300

    Agregué la conexión con la base de datos usando el localhost como pide el enunciado y eso. Además, cree la clase que se encarga de los métodos referidos a la base de datos, para no repetir la lógica de conectarse a la base, hacer las queris o lo que sea, etc. Tembién agregué un método que actualizaría el dataGridView pasando la query y la tabla a usar en la query. Todavía no lo probé porque estoy commiteando para pullear y no tenía cargado los datos en la base.

---
+++ src/AerolineaFrba/gdDataBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AerolineaFrba
+{
+    class gdDataBase
+    {
+        SqlConnection miConexion;
+
+        public void conectar()
+        {
+            miConexion = new SqlConnection("localhost\\SQLSERVER2012;Initial Catalog=GD2C2015;User ID=gd;Password=***********");
+            miConexion.Open();
+        }
+
+        public void desconectar()
+        {
+            miConexion.Close();
+        }
+
+        public void ejecutarSql(String query)
+        {
+            SqlCommand comando = new SqlCommand(query, miConexion);
+
+            int filasAfectadas = comando.ExecuteNonQuery();
+
+            if (filasAfectadas > 0)
+            {
+                //Salió bien (o al menos algo) de la query ejecutada
+            }
+            else
+            {
+                //falló la query
+            }
+        }
+
+        public void actualizarDataGridView(DataGridView dataGrid, String nombreTabla, String query)
+        {
+            conectar();
+
+            DataSet dataSet = new DataSet();
+
+            SqlDataAdapter adaptador = new SqlDataAdapter(query, miConexion);
+
+            adaptador.Fill(dataSet, nombreTabla);
+
+            dataGrid.DataSource = dataSet;
+            dataGrid.DataMember = nombreTabla;
+
+            desconectar();
+        }
+    }
+}
