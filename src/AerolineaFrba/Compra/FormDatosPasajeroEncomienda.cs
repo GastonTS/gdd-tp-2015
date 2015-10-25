861b79c39ab2e3523997efcf16c0a4b9482b2467
j9794 <juanmanuel9794@hotmail.com>
2015-10-25 16:19:48 -0300

    Empezando formIngresoDatosPasajero/Encomienda.

--- src/AerolineaFrba/Compra/FormDatosPasajeroEncomienda.cs
+++ src/AerolineaFrba/Compra/FormDatosPasajeroEncomienda.cs
@@ -16,5 +16,10 @@ namespace AerolineaFrba.Compra
         {
             InitializeComponent();
         }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
