f76c179aa77ce5f0ce2fdbe347a521daaca998d4
j9794 <juanmanuel9794@hotmail.com>
2015-10-30 12:11:34 -0300

    TextBoxNumeros.

---
+++ src/AerolineaFrba/Abm/TextBoxNumeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm
+{
+    public partial class TextBoxNumeros : AerolineaFrba.Abm.TextBoxValidado
+    {
+        public TextBoxNumeros()
+        {
+            InitializeComponent();
+        }
+
+        protected override String validationRegexString()
+        {
+            return "[0-9]{1,20}";
+        }
+    }
+}
