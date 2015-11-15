60bc77d69478a849e527ec9c0fdfbf4d04e78f6e
GastonTS <gastonsantoalla@gmail.com>
2015-11-14 23:34:23 -0300

    Cancelar pasajes de aeronave entre fechas (no optimized)

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -552,6 +552,55 @@ AS
 ;
 GO
 
+CREATE PROCEDURE ÑUFLO.CancelarPasajesDe
+@id_aeronave int,
+@fecha_hoy nvarchar(255),
+@fecha_inicio nvarchar(255),
+@fecha_fin nvarchar(255) = null
+AS
+	DECLARE @fecha_i datetime, @fecha_f datetime, @hoy datetime
+	SET @hoy = convert(datetime, @fecha_hoy)
+	SET @fecha_i = convert(datetime, @fecha_inicio)
+	SET @fecha_f = convert(datetime, @fecha_fin)
+
+	DECLARE CPasajes CURSOR 
+		FOR select c.codigo_de_compra, p.id_pasaje_encomienda
+				from ÑUFLO.Viaje v, ÑUFLO.Compra c, ÑUFLO.PasajeEncomienda p
+				where @id_aeronave = v.id_aeronave
+					and ((@fecha_f is null and v.fecha_salida > @fecha_i)
+					or v.fecha_salida between @fecha_i and @fecha_f)
+					and v.id_viaje = c.id_viaje
+					and c.codigo_de_compra = p.codigo_de_compra
+
+	DECLARE @pnr int, @pasaje int, @cod_anterior int
+	SET @cod_anterior = -1
+	OPEN CPasajes
+	FETCH CPasajes INTO @pnr, @pasaje
+
+	WHILE (@@FETCH_STATUS = 0)
+	BEGIN	
+		if(@pnr <> @cod_anterior)
+		BEGIN
+			INSERT INTO ÑUFLO.Cancelacion(codigo_de_compra, fecha_devolucion)
+				values(@pnr, @hoy)
+			SET @cod_anterior = @pnr
+		END
+
+		INSERT INTO ÑUFLO.PasajeEncomiendaPorCancelacion(id_cancelacion, id_pasaje_encomienda, motivo_cancelacion)
+			values((select MAX(id_cancelacion) from ÑUFLO.Cancelacion), @pasaje, 'Baja de Aeronave')
+
+		UPDATE ÑUFLO.PasajeEncomienda
+			SET cancelado = 1
+			WHERE @pasaje = id_pasaje_encomienda
+
+		FETCH CPasajes INTO @pnr, @pasaje
+	END
+
+	CLOSE CPasajes
+	DEALLOCATE CPasajes
+;
+GO
+
 CREATE PROCEDURE ÑUFLO.PesoDisponible
 @Id_viaje int
 AS
