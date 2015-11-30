c2bc76a485081148dbd4e2183d80255bebfa8c99
J9794 <juanmanuel9794@hotmail.com>
2015-11-30 16:22:45 -0300

    Refactor pedido por juan.

---
+++ src/AerolineaFrba/Abm/FormReloaded.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm
+{
+    public partial class FormReloaded : Form
+    {
+        public FormReloaded()
+        {
+            InitializeComponent();
+        }
+
+        protected void agregarValorCBADiccionario(ComboBox cb, Dictionary<String, gdDataBase.ValorTipo> camposValores,String nombreParam, SqlDbType tipoParam) 
+        {
+            if (cb.SelectedValue.ToString() != "")
+                camposValores.Add(nombreParam, new gdDataBase.ValorTipo(cb.SelectedValue, tipoParam));
+        }
+    }
+}
