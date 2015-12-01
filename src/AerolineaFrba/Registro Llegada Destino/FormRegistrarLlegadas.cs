e0ab383c0ef39439ce83f5b75a66c403d6085c91
J9794 <juanmanuel9794@hotmail.com>
2015-12-01 05:20:39 -0300

    Form registrar llegada llama al otro form.

--- src/AerolineaFrba/Registro Llegada Destino/FormRegistrarLlegadas.cs
+++ src/AerolineaFrba/Registro Llegada Destino/FormRegistrarLlegadas.cs
@@ -35,8 +35,13 @@ namespace AerolineaFrba.Registro_Llegada_Destino
 
         private void btnConfirmarLlegada_Click(object sender, EventArgs e)
         {
-            if (ValidateChildren())
+                        if (ValidateChildren())
             {
+                var informeYValidacion = new FormInformeYValidacion();
+                informeYValidacion.setAeronave(textBoxMatricula.Text);
+                informeYValidacion.setFecha(fechaCoso.Value);
+                informeYValidacion.setOrigen(comboBoxOrigen.SelectedValue.ToString());
+                informeYValidacion.setDestino(comboBoxDestino.SelectedValue.ToString());
                 var camposValores = gdDataBase.newParameters();
                 camposValores.Add("matricula", new gdDataBase.ValorTipo(textBoxMatricula, SqlDbType.NVarChar));
                 camposValores.Add("origen", new gdDataBase.ValorTipo(comboBoxOrigen.SelectedValue, SqlDbType.NVarChar));
