92bf36811c40048d7a3b6e675bc8a2cc8bc50a94
Ramiro Vazquez <ramirov93@gmail.com>
2015-12-01 14:09:46 -0300

    Defino unos structs para la compra de pasajes que voy a utilizar en un futuro

--- src/AerolineaFrba/Compra/FormDatosPasajeroEncomienda.Designer.cs
+++ src/AerolineaFrba/Compra/FormDatosPasajeroEncomienda.Designer.cs
@@ -183,6 +183,7 @@
             this.btnAceptar.TabIndex = 18;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // FormDatosPasajeroEncomienda
             // 
