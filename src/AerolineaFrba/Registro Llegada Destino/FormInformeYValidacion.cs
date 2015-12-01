22369371206fbd1245917614c1185ef24afe8ffa
J9794 <juanmanuel9794@hotmail.com>
2015-12-01 05:54:28 -0300

    Vinculo los datos del avion a los labels.

--- src/AerolineaFrba/Registro Llegada Destino/FormInformeYValidacion.cs
+++ src/AerolineaFrba/Registro Llegada Destino/FormInformeYValidacion.cs
@@ -19,7 +19,15 @@ namespace AerolineaFrba.Registro_Llegada_Destino
 
         public void setAeronave(String matricula) 
         {
-            MessageBox.Show("QUIERO UN SP QUE DADA UNA MATRICULA ME DE LOS DATOS DE UN AVION");
+            var camposValores = gdDataBase.newParameters();
+            camposValores.Add("matricula",new gdDataBase.ValorTipo(matricula,SqlDbType.NVarChar));
+            DataRow aeronave = new gdDataBase().ExecAndGetData("ÑUFLO.AeronavePorMatricula",camposValores, new Dictionary<int,string>()).Rows[0];
+            lblModelo.Text = aeronave["Modelo"].ToString();
+            lblMatricula.Text = aeronave["Matricula"].ToString();
+            lblFabricante.Text = aeronave["Fabricante"].ToString();
+            lblTipoServicio.Text = aeronave["Tipo servicio"].ToString();
+            lblCantButacas.Text = aeronave["Cantidad butacas"].ToString();
+            lblCapacEncomiendas.Text = aeronave["Capacidad encomiendas"].ToString();
         }
 
         public void setFecha(DateTime fecha)
--- src/AerolineaFrba/Registro Llegada Destino/FormRegistrarLlegadas.cs
+++ src/AerolineaFrba/Registro Llegada Destino/FormRegistrarLlegadas.cs
@@ -35,19 +35,22 @@ namespace AerolineaFrba.Registro_Llegada_Destino
 
         private void btnConfirmarLlegada_Click(object sender, EventArgs e)
         {
-                        if (ValidateChildren())
+                       if (ValidateChildren())
             {
-                var informeYValidacion = new FormInformeYValidacion();
-                informeYValidacion.setAeronave(textBoxMatricula.Text);
-                informeYValidacion.setFecha(fechaCoso.Value);
-                informeYValidacion.setOrigen(comboBoxOrigen.SelectedValue.ToString());
-                informeYValidacion.setDestino(comboBoxDestino.SelectedValue.ToString());
                 var camposValores = gdDataBase.newParameters();
                 camposValores.Add("matricula", new gdDataBase.ValorTipo(textBoxMatricula, SqlDbType.NVarChar));
                 camposValores.Add("origen", new gdDataBase.ValorTipo(comboBoxOrigen.SelectedValue, SqlDbType.NVarChar));
                 camposValores.Add("destino", new gdDataBase.ValorTipo(comboBoxDestino.SelectedValue, SqlDbType.NVarChar));
                 camposValores.Add("fecha_llegada", new gdDataBase.ValorTipo(fechaCoso.Value, SqlDbType.DateTime));
-                new gdDataBase().Exec("ÑUFLO.RegistrarLlegada", camposValores, new Dictionary<int, string>());
+                if (new gdDataBase().Exec("ÑUFLO.ValidarRegistroLlegada", camposValores, new Dictionary<int, string>()))
+                {
+                    var informeYValidacion = new FormInformeYValidacion();
+                    informeYValidacion.setAeronave(textBoxMatricula.Text);
+                    informeYValidacion.setFecha(fechaCoso.Value);
+                    informeYValidacion.setOrigen(comboBoxOrigen.SelectedValue.ToString());
+                    informeYValidacion.setDestino(comboBoxDestino.SelectedValue.ToString());
+                    informeYValidacion.Show();                    
+                }
             }
         }
     }
