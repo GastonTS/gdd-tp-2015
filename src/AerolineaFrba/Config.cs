a587447561e96f5634a31edfc7f5620470a840d0
Ramiro Vazquez <ramirov93@gmail.com>
2015-12-02 17:57:50 -0300

    Primer intento de hacer las fechas con el archivo config

---
+++ src/AerolineaFrba/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba
+{
+    class Config
+    {
+        public static string strConnection = ConfigurationManager.ConnectionStrings["AerolineaFrba.Properties.Settings.GD2C2015ConnectionString"].ConnectionString;
+        public static DateTime fecha = DateTime.Parse(ConfigurationManager.AppSettings["Fecha"]);
+        public static String terminal = ConfigurationManager.AppSettings["Terminal"];
+    }
+}
