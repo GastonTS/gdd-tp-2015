51f826ad71b21711d49f632569630ad96778450c
JuanchiRios <juanchirios04@hotmail.com>
2015-12-04 14:07:58 -0300

    custom format datePicker formSelectionAeronave

--- src/AerolineaFrba/Abm Aeronave/FormSeleccionAeronave.Designer.cs
+++ src/AerolineaFrba/Abm Aeronave/FormSeleccionAeronave.Designer.cs
@@ -295,6 +295,7 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(567, 539);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(162, 20);
