2cfd4ee613514b311d60d1fd6edf4d98839595ac
J9794 <juanmanuel9794@hotmail.com>
2015-12-04 09:04:05 -0300

    Canje millas mas detallado.

--- src/AerolineaFrba/Canje Millas/FormCajeMillasPasajero.cs
+++ src/AerolineaFrba/Canje Millas/FormCajeMillasPasajero.cs
@@ -43,6 +43,32 @@ namespace AerolineaFrba.Canje_Millas
             new gdDataBase().Exec("ÑUFLO.CanjearProductoA", camposValores, errorMensaje, "Canje realizado con éxito");
         }
 
+        private int cantidadProducto() 
+        {
+            if (textBoxCantidadProducto.Text.Trim() == "") return 0;
+            else return int.Parse(textBoxCantidadProducto.Text);
+        }
+
+        private void actualizarDatosTransaccion()
+        {
+            if (comboBoxProducto.Items.Count != 0)
+            {
+                var camposValores = gdDataBase.newParameters();
+                camposValores.Add("nombre",new gdDataBase.ValorTipo(comboBoxProducto.SelectedValue.ToString(),SqlDbType.NVarChar);
+                var dt = new gdDataBase().GetDataWithParameters("ÑUFLO.DatosDeProducto",camposValores);
+                var precioUnitario = (int)dt.Rows[0]["Precio"];
+                var stock = (int)dt.Rows[0]["Stock"];
+                var precioTotal = precioUnitario * cantidadProducto();
+                var millasRestantes = int.Parse(lblCantMillas.Text) - precioTotal;
+                lblCostoProducto.Text = precioUnitario.ToString();
+                lblStock.Text = stock.ToString();
+                lblCostoTotal.Text = precioTotal.ToString();
+                lblMillasRestantes.Text = precioTotal.ToString();
+            }
+
+
+        }
+
         private void btnVerProductos_Click(object sender, EventArgs e)
         {
             Dictionary<String, gdDataBase.ValorTipo> camposValores = new Dictionary<string, gdDataBase.ValorTipo>();
@@ -57,7 +83,29 @@ namespace AerolineaFrba.Canje_Millas
 
                 comboBoxProducto.Enabled = true;
                 textBoxCantidadProducto.Enabled = true;
+
+                actualizarDatosTransaccion();
             }
+        }
+
+        private void textBoxDni_TextboxTextChanged(object sender, EventArgs e)
+        {
+            if (textBoxDni.Text.Trim() != "")
+            {
+                var camposValores = gdDataBase.newParameters();
+                camposValores.Add("dni", new gdDataBase.ValorTipo(textBoxDni.Text, SqlDbType.Int));
+                lblCantMillas.Text = new gdDataBase().ExecAndGetData("ÑUFLO.MillasTotalesDe", camposValores, new Dictionary<int, String>()).Rows[0].ItemArray[0].ToString();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
