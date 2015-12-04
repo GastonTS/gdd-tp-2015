c8a1749719e0a5ef9bab5c3474876f53e854828c
J9794 <juanmanuel9794@hotmail.com>
2015-12-04 11:33:52 -0300

    Limpiando selectssss.

--- src/AerolineaFrba/Abm Ruta/FormAltaRuta.cs
+++ src/AerolineaFrba/Abm Ruta/FormAltaRuta.cs
@@ -49,29 +49,6 @@ namespace AerolineaFrba.Abm_Ruta
             }
         }
 
-        public FormAltaRuta(DataRowView datosAModificar)
-        {
-            InitializeComponent();
-
-            var ds = new gdDataBase().GetDataSP("ÑUFLO.CiudadTipoServicio");
-
-            origenBinding.DataSource = ds.Tables[0];
-            destinoBinding.DataSource = ds.Tables[0];
-            tipoServicioBinding.DataSource = ds.Tables[1];
-
-            comboBoxOrigen.DisplayMember = "Nombre";
-            comboBoxOrigen.ValueMember = "Id ciudad";
-            comboBoxDestino.DisplayMember = "Nombre";
-            comboBoxDestino.ValueMember = "Id ciudad";
-            comboBoxTipoServicio.DisplayMember = "Tipo Servicio";
-            comboBoxTipoServicio.ValueMember = "Id Tipo Servicio";
-
-            textBoxCodRuta.Text = datosAModificar["Código Ruta"].ToString();
-            DataSet dss = new gdDataBase().GetDataQuery("Select id_ciudad FROM ÑUFLO.Ciudad where nombre= " + "'" + datosAModificar["Ciudad Origen"].ToString() + "'");
-            comboBoxOrigen.SelectedIndex = dss.Tables[0].Rows[0].Field<int>(0) - 1;
-            dss = new gdDataBase().GetDataQuery("Select id_ciudad FROM ÑUFLO.Ciudad where nombre= " + "'" + datosAModificar["Ciudad Destino"].ToString() + "'");
-            comboBoxDestino.SelectedIndex = dss.Tables[0].Rows[0].Field<int>(0) - 1;
-        }
 
         public void setId(int id)
         {
