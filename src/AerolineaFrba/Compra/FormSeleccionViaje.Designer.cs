59edd5089f2f56209a9e891c4ea37987a038f703
Ramiro Vazquez <ramirov93@gmail.com>
2015-12-10 18:26:24 -0300

    Cambio propiedad de multiSelect a false

--- src/AerolineaFrba/Compra/FormSeleccionViaje.Designer.cs
+++ src/AerolineaFrba/Compra/FormSeleccionViaje.Designer.cs
@@ -118,6 +118,7 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(25, 203);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(563, 154);
