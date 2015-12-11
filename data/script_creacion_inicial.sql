b10d55a6e30d12b00b15dbc36db26b384c367971
JuanchiRios <juanchirios04@hotmail.com>
2015-12-11 14:04:26 -0300

    fixbug no testeo una verga

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -1094,6 +1094,7 @@ AS
 				and ((@fecha_f is null and fecha_salida > @fecha_i)
 				or fecha_salida between @fecha_i and @fecha_f)
 				and cancelado = 0
+				and fecha_llegada is null
 
 	BEGIN TRANSACTION
 	
@@ -1189,7 +1190,7 @@ AS
 	IF(@fecha_llegada_estimada < @fecha_salida)
 		THROW 60013, 'La fecha de llegada no puede ser menor a la de salida', 1
 		
-	IF(@fecha_salida < @hoy)
+	IF(@fecha_salida <= @hoy)
 		THROW 60014, 'La fecha de salida debe ser mayor a la fecha de hoy', 1
 		
 	IF ((select id_tipo_servicio from ÑUFLO.ServicioPorRuta where id_ruta = @id_ruta) <> (select id_tipo_servicio from ÑUFLO.Aeronave where id_aeronave = @id_aeronave))
@@ -1782,13 +1783,14 @@ AS
 			set cancelado = 1
 		WHERE id_ruta=@id_ruta;
 		
+		EXEC ÑUFLO.CancelarRutaAerea @id_ruta, @hoy
 			
 		UPDATE ÑUFLO.Viaje
 			SET cancelado = 1
 			where id_ruta = @id_ruta;
 		END
 	
-	EXEC ÑUFLO.CancelarRutaAerea @id_ruta, @hoy
+	
 ;  
 GO
 
