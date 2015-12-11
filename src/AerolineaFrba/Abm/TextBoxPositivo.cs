9dc7e944f4789d192f497c36477a940b313a93e5
JuanchiRios <juanchirios04@hotmail.com>
2015-12-11 15:01:27 -0300

    nuevo textBox numeros mayor a cero

---
+++ src/AerolineaFrba/Abm/TextBoxPositivo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba.Abm
+{
+    class TextBoxPositivo : TextBoxDecimal
+    {
+        protected override bool criterioValidacion()
+        {
+            if (base.criterioValidacion())
+                return Double.Parse(textBox1.Text) > 0;
+            else
+                return false;
+        }
+    }
+}
